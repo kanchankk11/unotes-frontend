841736476b0ecf2fc3730aeffa52bd4a31e7e34c
kanchankk11 <nagkanchan5@gmail.com>
2023-04-07 20:11:51 +0530

    Navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
                 <div class="container-fluid">
                     <a class="navbar-brand" href="#">
                         <img src="logo.png" alt="" width="30" height="24" class="d-inline-block align-text-top" />
-                        Bootstrap
+                        uNotes
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
